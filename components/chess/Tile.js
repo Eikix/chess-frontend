3f4c766e7a05eb11797e2e573600e51f7246605d
Eikix <66871571+Eikix@users.noreply.github.com>
2021-08-17 17:36:24 +0200

    adjusted h & w

--- components/chess/Tile.js
+++ components/chess/Tile.js
@@ -1,7 +1,7 @@
 const Tile = ({color, children}) => {
     const myColor = color;
     return (
-        <div className={`flex justify-center cursor-pointer items-center h-9 w-9 md:h-12 md:w-12 lg:h-16 lg:w-16 xl:h-20 xl:w-20 border ${color === "blue" ? "bg-blue-100" : (color === "gray" ? "bg-gray-100" : "bg-green-100")}`}>
+        <div className={`flex justify-center cursor-pointer items-center h-10 w-10 md:h-12 md:w-12 lg:h-16 lg:w-16 xl:h-20 xl:w-20 border ${color === "blue" ? "bg-blue-100" : (color === "gray" ? "bg-gray-100" : "bg-green-100")}`}>
             {children}
         </div>
     )
