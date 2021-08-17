f3b137a0de7ac225f63c9ead6e5064781d600382
Eikix <66871571+Eikix@users.noreply.github.com>
2021-08-17 17:27:08 +0200

    lowered padding x axis

--- components/chess/BoardLayout.js
+++ components/chess/BoardLayout.js
@@ -1,6 +1,6 @@
 const BoardLayout = ({children}) => {
     return (
-        <div className="h-full grid grid-cols-8 px-6 py-16 md:px-32 lg:px-46 xl:px-80">
+        <div className="h-full grid grid-cols-8 px-3 py-16 md:px-32 lg:px-46 xl:px-80">
             {children}
         </div>
     )
