592ced8c64712d944a4bb1b4e6612c34ed94af05
Eikix <66871571+Eikix@users.noreply.github.com>
2021-10-05 22:49:48 +0200

    added new game

--- pages/index.js
+++ pages/index.js
@@ -1,87 +1,120 @@
-import Board from "../components/chess/Board"
-import * as Chess from "chess.js";
-import { useEffect, useState } from "react";
-import {socket} from "../utils/socket/socket";
-
-
+import Board from '../components/chess/Board';
+import * as Chess from '../utils/chessLibrary/chessmythJS/chess/CMChess';
+import { useEffect, useState } from 'react';
+import { socket } from '../utils/socket/socket';
+import piecesMoveset from '../utils/chessLibrary/chessmythJS/chess/utils/pieces/pieceTypeToMoves';
 
 const Home = () => {
-  const [chess, setChess] = useState();
-  const [chessBoard, setChessBoard] = useState([]);
-  const [isWhiteTurn, setWhiteTurn] = useState(true);
-  const [roomId, setRoomId] = useState("");
-  const [playerColor, setPlayerColor] = useState(null);
-  const [isJoining, setIsJoining] = useState(false);
+    const [chess, setChess] = useState();
+    const [chessBoard, setChessBoard] = useState([]);
+    const [isWhiteTurn, setWhiteTurn] = useState(true);
+    const [roomId, setRoomId] = useState('');
+    const [playerColor, setPlayerColor] = useState(null);
+    const [isJoining, setIsJoining] = useState(false);
 
-  const handleTurnChange = () => {
-    setWhiteTurn(prevTurn => !prevTurn);
-  }
+    const width = 10;
+    const boardOptions = {
+        backline: 'rnbbqkbnnr',
+        frontline: 'pppppppppp',
+        width: width,
+        terrain: ['4;6'],
+        mana: ['3;6', '7;8'],
+        wKing: '6;1',
+        bKing: '6;10',
+    };
 
-  const joinChessRoom = (event) => {
-    event.preventDefault();
-    setIsJoining(true);
-    // console.log(roomId);
-    socket.emit("join_room", roomId);
-  }
+    const handleTurnChange = () => {
+        setWhiteTurn(prevTurn => !prevTurn);
+    };
 
-  const handleRoomId = (event) => {
-    const currentRoomId = event.target.value;
-    setRoomId(currentRoomId);
-  }
+    const joinChessRoom = event => {
+        event.preventDefault();
+        setIsJoining(true);
+        // console.log(roomId);
+        socket.emit('join_room', roomId);
+    };
 
-  useEffect( () => {
-    setChessBoard(() =>{
-      if (chess) return chess.board();
-    });
-  }, [chess, isWhiteTurn]);
+    const handleRoomId = event => {
+        const currentRoomId = event.target.value;
+        setRoomId(currentRoomId);
+    };
 
-  useEffect(() => {
-    socket.on("game_start", res => {
-      console.log("Game Starting: ", chess);
-      if (!res.error && chess === undefined) {
-        const newChess = new Chess();
-        setChess(newChess);
-      }
-    });
+    useEffect(() => {
+        setChessBoard(() => {
+            if (chess) return chess?.getBoard();
+        });
+    }, [chess, isWhiteTurn]);
 
-    socket.on("on_join", msg => {
-      console.log(msg);
-      setPlayerColor(msg.color);
-    });
-  }, [])
-  
-
-
-  return (
-      <div>
-        <div className="flex flex-col items-center justify-center">
-          <h1 className="text-2xl md:text-3xl lg:text-4xl xl:text-5xl mt-6 border-b shadow-sm">Eiki's Chess</h1>
-
-          {(chess && chess?.game_over()) && 
-            <div className="text-3xl cursor-pointer bg-blue-100 text-gray-800 rounded-full p-6 font-light text-center mt-6 animate-pulse">
-              <p>Game over!</p>
-            </div>
-          }
-
-          {!chessBoard && 
-            <form className="flex flex-col justify-center items-center mt-24 space-y-4 md:space-y-6 text-xl lg:text-2xl xl:text-3xl font-light" onSubmit={joinChessRoom}>
-              <label htmlFor="roomId">Room Id</label>
-              <input type="text" id="roomId" name="roomId" value={roomId} onChange={handleRoomId} className="p-3 rounded-lg font-light border border-blue-900" placeholder="Enter Room Id"/>
-              <button type="submit" disabled={isJoining} className="bg-blue-50 p-3 lg:p-6 rounded-lg font-light">{!isJoining ? "Join Room": "Awaiting player 2..."}</button>
-            </form>
+    useEffect(() => {
+        socket.on('game_start', res => {
+            console.log('Game Starting: ', chess);
+            if (!res.error && chess === undefined) {
+                const newChess = new Chess(boardOptions, piecesMoveset);
+                setChess(newChess);
             }
-          {(chess && chessBoard) && <Board chessMatrix={chessBoard} chess={chess} isWhiteTurn={isWhiteTurn} handleTurnChange={handleTurnChange} playerColor={playerColor}/>}
+        });
 
+        socket.on('on_join', msg => {
+            console.log(msg);
+            setPlayerColor(msg.color);
+        });
+    }, []);
 
-          {/* {console.log(chess)}
+    return (
+        <div>
+            <div className="flex flex-col items-center justify-center">
+                <h1 className="text-2xl md:text-3xl lg:text-4xl xl:text-5xl mt-6 border-b shadow-sm">
+                    Eiki's Chess Myth
+                </h1>
+
+                {chess && chess?.isGameOver && (
+                    <div className="text-3xl cursor-pointer bg-blue-100 text-gray-800 rounded-full p-6 font-light text-center mt-6 animate-pulse">
+                        <p>Game over!</p>
+                    </div>
+                )}
+
+                {!chessBoard && (
+                    <form
+                        className="flex flex-col justify-center items-center mt-24 space-y-4 md:space-y-6 text-xl lg:text-2xl xl:text-3xl font-light"
+                        onSubmit={joinChessRoom}
+                    >
+                        <label htmlFor="roomId">Room Id</label>
+                        <input
+                            type="text"
+                            id="roomId"
+                            name="roomId"
+                            value={roomId}
+                            onChange={handleRoomId}
+                            className="p-3 rounded-lg font-light border border-blue-900"
+                            placeholder="Enter Room Id"
+                        />
+                        <button
+                            type="submit"
+                            disabled={isJoining}
+                            className="bg-blue-50 p-3 lg:p-6 rounded-lg font-light"
+                        >
+                            {!isJoining ? 'Join Room' : 'Awaiting player 2...'}
+                        </button>
+                    </form>
+                )}
+                {chess && chessBoard && (
+                    <Board
+                        chessMatrix={chessBoard}
+                        chess={chess}
+                        isWhiteTurn={isWhiteTurn}
+                        handleTurnChange={handleTurnChange}
+                        playerColor={playerColor}
+                    />
+                )}
+
+                {/* {console.log(chess)}
           {console.log(chessBoard)} */}
-          
-        </div>
-        {/* <div className="flex items-center justify-center mb-6 lg:mb-12">
+            </div>
+            {/* <div className="flex items-center justify-center mb-6 lg:mb-12">
           {chessBoard && <button className="bg-blue-50 p-3 rounded-lg" onClick={startChessGame}>Reset game</button> }
         </div> */}
-      </div>
-  )
-}
+        </div>
+    );
+};
 
-export default Home
+export default Home;
--- utils/socket/socket.js
+++ utils/socket/socket.js
@@ -1,9 +1,7 @@
-import io from "socket.io-client";
+import io from 'socket.io-client';
 
-const URL = "https://eikichess-server.herokuapp.com/";
+const URL = 'https://chessmyth-server.herokuapp.com/';
 
 const socket = io(URL);
 
-export {
-    socket
-}
+export { socket };
